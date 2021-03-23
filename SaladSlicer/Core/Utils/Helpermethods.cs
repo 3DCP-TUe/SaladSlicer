2553e64904ed36f7aeab9cba91b591ae8e07deb7
Arjen Deetman <info@arjendeetman.nl>
2021-03-23 16:30:16 +0100

    Added serialization and deserializing methods

--- SaladSlicer/Core/Utils/Helpermethods.cs
+++ SaladSlicer/Core/Utils/Helpermethods.cs
@@ -1,9 +1,8 @@
 ﻿// System Libs
 using System;
+using System.IO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace SaladSlicer.Core.Utils
 {
@@ -12,5 +11,41 @@ namespace SaladSlicer.Core.Utils
     /// </summary>
     public static class Helpermethods
     {
+        /// <summary>
+        /// Serializes a common object to a byte array. 
+        /// Typically used for serializing meshes and data inside Goo classes.
+        /// Taken from <https://github.com/RobotComponents/RobotComponents> (GPL v3).
+        /// </summary>
+        /// <param name="obj"> The common object. </param>
+        /// <returns> The byte array. </returns>
+        public static byte[] ObjectToByteArray(Object obj)
+        {
+            if (obj == null) { return null; }
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(stream, obj);
+                return stream.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Deserializes a byte array to a common object. 
+        /// Typically used for deserializing meshes and data inside Goo classes.
+        /// Taken from <https://github.com/RobotComponents/RobotComponents> (GPL v3).
+        /// </summary>
+        /// <param name="data"> The byte array. </param>
+        /// <returns> The common object. </returns>
+        public static Object ByteArrayToObject(byte[] data)
+        {
+            using (MemoryStream stream = new MemoryStream(data))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                stream.Write(data, 0, data.Length);
+                stream.Seek(0, SeekOrigin.Begin);
+                return (Object)formatter.Deserialize(stream);
+            }
+        }
     }
 }
