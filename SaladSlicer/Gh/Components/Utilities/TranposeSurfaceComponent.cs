ce91d3f52a26329eb2f7fdf583764a43676713bf
Arjen Deetman <arjendeetman@hotmail.com>
2021-03-26 16:02:55 +0100

    Added the tranpose surface component

---
+++ SaladSlicer/Gh/Components/Utilities/TranposeSurfaceComponent.cs
@@ -0,0 +1,92 @@
+﻿// System Libs
+using System;
+// Grasshopper Libs
+using Grasshopper.Kernel;
+// Rhino Libs
+using Rhino.Geometry;
+
+namespace SaladSlicer.Gh.Components.Utilities
+{
+    public class TransposeSurfaceComponent : GH_Component
+    {
+        /// <summary>
+        /// Public constructor without any arguments.
+        /// </summary>
+        public TransposeSurfaceComponent()
+          : base("Transpose Surface", // Component name
+              "TS", // Component nickname
+              "Tranpose a surface (swap U and V).", // Description
+              "Salad Slicer", // Category
+              "Utilities") // Subcategory
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddSurfaceParameter("Surface", "S", "Surface as a Surface.", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddSurfaceParameter("Surface", "S", "Surface as a Surface", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            // Declare variable of input parameters
+            Surface surface = Surface.CreateExtrusion(new Line(0.0, 0.0, 0.0, 0.0, 0.0, 1.0).ToNurbsCurve(), new Vector3d(1.0, 0.0, 0.0));
+
+            // Access the input parameters individually. 
+            if (!DA.GetData(0, ref surface)) return;
+
+            // Transpose the surface
+            Surface surface2 = surface.Transpose();
+
+            // Assign the output parameters
+            DA.SetData(0, surface2);
+        }
+
+        /// <summary>
+        /// Gets the exposure of this object in the Graphical User Interface.
+        /// </summary>
+        public override GH_Exposure Exposure
+        {
+            get { return GH_Exposure.primary; }
+        }
+
+        /// <summary>
+        /// Gets whether this object is obsolete.
+        /// </summary>
+        public override bool Obsolete
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Gets the 24x24 pixels icon.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get { return Properties.Resources.ExampleIcon; }
+        }
+
+        /// <summary>
+        /// Gets the ID for this object type. 
+        /// Every object must supply a unique and unchanging ID that is used to identify objects of the same type.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("F1BDDB61-EBBE-4D93-B5A7-E91FBB5D87D6"); }
+        }
+    }
+}
