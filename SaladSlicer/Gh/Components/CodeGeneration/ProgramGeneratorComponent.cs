c713a48965a7c4d99abc97db204b5313ba6db86d
Arjen Deetman <info@arjendeetman.nl>
2021-04-30 17:56:33 +0200

    Added the Code Line and Program Generator classes and components

---
+++ SaladSlicer/Gh/Components/CodeGeneration/ProgramGeneratorComponent.cs
@@ -0,0 +1,102 @@
+﻿// This file is part of SaladSlicer. SaladSlicer is licensed 
+// under the terms of GNU General Public License as published by the 
+// Free Software Foundation. For more information and the LICENSE file, 
+// see <https://github.com/3DCP-TUe/SaladSlicer>.
+
+// System Libs
+using System;
+using System.Collections.Generic;
+// Grasshopper Libs
+using Grasshopper.Kernel;
+// Salad Slicer Libs
+using SaladSlicer.Core.CodeGeneration;
+
+namespace SaladSlicer.Gh.Components.CodeGeneration
+{
+    /// <summary>
+    /// Represent a component that creates the program.
+    /// </summary>
+    public class ProgramGeneratorComponent : GH_Component
+    {
+        /// <summary>
+        /// Public constructor without any arguments.
+        /// </summary>
+        public ProgramGeneratorComponent()
+          : base("Program Generator", // Component name
+              "PG", // Component nickname
+              "Program Generator.", // Description
+              "Salad Slicer", // Category
+              "Code Generation") // Subcategory
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Objects", "O", "All objects that can generator G-code", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Progam", "P", "The program as a list with code lines", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            // Declare variable of input parameters
+            List<IGCode> objects = new List<IGCode>();
+
+            // Access the input parameters individually. 
+            if (!DA.GetDataList(0, objects)) return;
+
+            // Create the line
+            ProgamGenerator programGenerator = new ProgamGenerator();
+            List<string> program = programGenerator.CreateProgram(objects);
+
+            // Assign the output parameters
+            DA.SetDataList(0, program);
+        }
+
+        /// <summary>
+        /// Gets the exposure of this object in the Graphical User Interface.
+        /// </summary>
+        public override GH_Exposure Exposure
+        {
+            get { return GH_Exposure.primary; }
+        }
+
+        /// <summary>
+        /// Gets whether this object is obsolete.
+        /// </summary>
+        public override bool Obsolete
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Gets the 24x24 pixels icon.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get { return Properties.Resources.ExampleIcon; }
+        }
+
+        /// <summary>
+        /// Gets the ID for this object type. 
+        /// Every object must supply a unique and unchanging ID that is used to identify objects of the same type.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("A387F8ED-44EF-4157-983C-41C88042A67F"); }
+        }
+    }
+}
