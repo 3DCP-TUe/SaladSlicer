383671b2cadf2be984b1ae436c02286e0ecbb1d2
Arjen Deetman <arjendeetman@hotmail.com>
2021-05-05 17:21:48 +0200

    Edited resource files

--- SaladSlicer/Properties/Resources.Designer.cs
+++ SaladSlicer/Properties/Resources.Designer.cs
@@ -79,15 +79,5 @@ namespace SaladSlicer.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap TwoHalfD {
-            get {
-                object obj = ResourceManager.GetObject("TwoHalfD", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
