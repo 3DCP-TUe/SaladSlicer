b7f015aaeaee96d5e15124ac5adb4b7d3ce03407
Derk <d.h.bos@tue.nl>
2021-05-31 18:11:05 +0200

    Simplified the comonents

--- SaladSlicer/Gh/Components/Geometry/JoinClosedCurvesComponent.cs
+++ SaladSlicer/Gh/Components/Geometry/JoinClosedCurvesComponent.cs
@@ -16,6 +16,7 @@ using Grasshopper.Kernel.Data;
 // Rhino Libs
 using Rhino.Geometry;
 using SaladSlicer.Core.Geometry;
+using SaladSlicer.Gh.Utils;
 using SaladSlicer.Core.Enumerations;
 
 namespace SaladSlicer.Gh.Components.Geometry
@@ -26,7 +27,7 @@ namespace SaladSlicer.Gh.Components.Geometry
     public class JoinClosedCurvesComponent : GH_Component
     {
         #region fields
-        private bool _created = false;
+        private bool _expire = false;
         #endregion
 
         /// <summary>
@@ -47,11 +48,8 @@ namespace SaladSlicer.Gh.Components.Geometry
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
             pManager.AddCurveParameter("Curves", "C", "List of curves", GH_ParamAccess.list);
-            pManager.AddNumberParameter("Startparameter", "S", "A parameter thet redefines the startpoint of the curves [0 to 1]", GH_ParamAccess.item, 0);
-            pManager.AddNumberParameter("Changelength", "L", "Sets the length over which to connect to the next layer", GH_ParamAccess.item, 100);
-            pManager.AddBooleanParameter("Alternate", "A", "Reverses the direction of every other curve", GH_ParamAccess.item,false);
             pManager.AddIntegerParameter("Connection type", "T", "Sets the type of connection [0 = Linear,1 = Bezier]", GH_ParamAccess.item,0);
-
+            pManager.AddNumberParameter("Changelength", "L", "Sets the length over which to connect to the next layer", GH_ParamAccess.item, 100);
         }
 
         /// <summary>
@@ -68,30 +66,28 @@ namespace SaladSlicer.Gh.Components.Geometry
         /// <param name="DA">The DA object can be used to retrieve data from input parameters and to store data in output parameters.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
+            //Create a value list
+            _expire = HelperMethodsGh.CreateValueList(this, 1, typeof(Transition), _expire);
+            
             // Expire solution of this component
-            if (_created == false)
+            if (_expire == true)
             {
-                CreateValueList();
-                _created = true;
+                _expire = false;
+                this.ExpireSolution(true);
             }
+
             // Declare variable of input parameters
             List<Curve> curves = new List<Curve>();
-            bool reverse = new bool();
             int type =new int();
             Curve joinedCurve;
             double changeLength=new double();
-            double startParameter = new double();
 
             // Access the input parameters individually. 
             if (!DA.GetDataList(0, curves)) return;
-            if (!DA.GetData(1, ref startParameter)) return;
+            if (!DA.GetData(1, ref type)) return;
             if (!DA.GetData(2, ref changeLength)) return;
-            if (!DA.GetData(3, ref reverse)) return;
-            if (!DA.GetData(4, ref type)) return;
 
             // Check input values
-            if (startParameter < 0.0) { AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Parameter value is not in the range of 0 to 1."); }
-            if (startParameter > 1.0) { AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Parameter value is not in the range of 0 to 1."); }
             if (curves[0].GetLength() < changeLength) { AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "The length of the layer change exceeds the length of the base contour."); }
             if (Curves.NumberClosed(curves) != curves.Count) { AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "One or more curves are not closed"); }
             if (type != 0 && type != 1)
@@ -99,70 +95,40 @@ namespace SaladSlicer.Gh.Components.Geometry
                 AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Comment type value <" + type + "> is invalid. " +
                     "In can only be set to 0 or 1. Use 0 for linear and 1 for bezier connections.");
             }
+            
             // Create the code line
             List<Curve> curvesCopy = curves.ConvertAll(curve => curve.DuplicateCurve());
             curvesCopy = Curves.CutTransitionEnd(curves, changeLength);
+            
             if (type == 0)
             {
-                joinedCurve = Curves.JoinLinear(curvesCopy, reverse);
+                joinedCurve = Curves.JoinLinear(curvesCopy, false);
             }
             else if (type == 1)
             {
-                joinedCurve = Curves.JoinBezier(curvesCopy, reverse);
+                joinedCurve = Curves.JoinBezier(curvesCopy, false);
             }
             else
             {
                 joinedCurve = Line.Unset.ToNurbsCurve();
             }
 
+            /**
+            try
+            {
+                bool test = true;
+                test = false;
+            }
+            catch (Exception e)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, e.Message);
+            }
+            **/
+
             // Assign the output parameters
             DA.SetData(0, joinedCurve);
         }
-        #region valuelist
-        /// <summary>
-        /// Creates the value list for the transition type and connects it the input parameter is other source is connected
-        /// </summary>
-        private void CreateValueList()
-        {
-            if (this.Params.Input[4].SourceCount == 0)
-            {
-                var parameter = Params.Input[4];
-                // Creates the empty value list
-                GH_ValueList obj = new GH_ValueList();
-                obj.CreateAttributes();
-                obj.ListMode = GH_ValueListMode.DropDown;
-                obj.ListItems.Clear();
-                obj.ToggleItem(1);
-                // Add the items to the value list
-                string[] names = Enum.GetNames(typeof(Transition));
-                int[] values = (int[])Enum.GetValues(typeof(Transition));
 
-                for (int i = 0; i < names.Length; i++)
-                {
-                    obj.ListItems.Add(new GH_ValueListItem(names[i], values[i].ToString()));
-                }
-
-                //obj.SelectedItems.Remove(new GH_ValueListItem(names[0], values[0].ToString()));
-                //obj.SelectedItems.Add(new GH_ValueListItem(names[1], values[1].ToString()));
-                // Make point where the valuelist should be created on the canvas
-                obj.Attributes.Pivot = new PointF(this.Attributes.Pivot.X - this.Attributes.Bounds.Width - obj.Attributes.Bounds.Width / 4, this.Attributes.Pivot.Y + this.Attributes.Bounds.Height / 2 - obj.Attributes.Bounds.Height);
-                // Add the value list to the active canvas
-                Instances.ActiveCanvas.Document.AddObject(obj, false);
-
-                // Connect the value list to the input parameter
-                parameter.AddSource(obj);
-
-                // Collect data
-                parameter.CollectData();
-
-                // Created
-                _created = true;
-
-                // Expire value list
-                obj.ExpireSolution(true);
-            }
-        }
-        #endregion
         /// <summary>
         /// Gets the exposure of this object in the Graphical User Interface.
         /// </summary>
--- SaladSlicer/Gh/Components/Geometry/JoinOpenCurvesComponent.cs
+++ SaladSlicer/Gh/Components/Geometry/JoinOpenCurvesComponent.cs
@@ -17,6 +17,7 @@ using Grasshopper.Kernel.Data;
 using Rhino.Geometry;
 using SaladSlicer.Core.Geometry;
 using SaladSlicer.Core.Enumerations;
+using SaladSlicer.Gh.Utils;
 
 namespace SaladSlicer.Gh.Components.Geometry
 {
@@ -26,7 +27,7 @@ namespace SaladSlicer.Gh.Components.Geometry
     public class JoinOpenCurvesComponent : GH_Component
     {
         #region fields
-        private bool _created = false;
+        private bool _expire = false;
         #endregion
 
         /// <summary>
@@ -47,8 +48,7 @@ namespace SaladSlicer.Gh.Components.Geometry
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
             pManager.AddCurveParameter("Curves", "C", "List of curves", GH_ParamAccess.list);
-            pManager.AddBooleanParameter("Alternate", "A", "Reverses the direction of every other curve", GH_ParamAccess.item,true);
-            pManager.AddIntegerParameter("Connection type", "T", "Sets the type of connection [0 = Linear,1 = Bezier]", GH_ParamAccess.item,0);
+            pManager.AddIntegerParameter("Connection type", "T", "Sets the type of connection [0 = Linear,1 = Bezier]", GH_ParamAccess.item, 0);
         }
 
         /// <summary>
@@ -65,32 +65,33 @@ namespace SaladSlicer.Gh.Components.Geometry
         /// <param name="DA">The DA object can be used to retrieve data from input parameters and to store data in output parameters.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
+            //Create a value list
+            _expire = HelperMethodsGh.CreateValueList(this, 1, typeof(Transition), _expire);
+
             // Expire solution of this component
-            if (_created == false)
+            if (_expire == true)
             {
-                CreateValueList();
-                _created = true;
+                _expire = false;
+                this.ExpireSolution(true);
             }
 
             // Declare variable of input parameters
             List<Curve> curves = new List<Curve>();
-            bool reverse = new bool();
             int type = new int();
             Curve joinedCurve;
 
             // Access the input parameters individually. 
             if (!DA.GetDataList(0, curves)) return;
-            if (!DA.GetData(1, ref reverse)) return;
-            if (!DA.GetData(2, ref type)) return;
-            // Create the code line
-            
+            if (!DA.GetData(1, ref type)) return;
+
+            // Create the code line            
             if (type == 0)
             {
-                joinedCurve = Curves.JoinLinear(curves, reverse);
+                joinedCurve = Curves.JoinLinear(curves, false);
             }
             else if (type == 1)
             {
-                joinedCurve = Curves.JoinBezier(curves, reverse);
+                joinedCurve = Curves.JoinBezier(curves, false);
             }
             else
             {
@@ -100,49 +101,7 @@ namespace SaladSlicer.Gh.Components.Geometry
             // Assign the output parameters
             DA.SetData(0, joinedCurve);
         }
-        #region valuelist
-        /// <summary>
-        /// Creates the value list for the transition type and connects it the input parameter is other source is connected
-        /// </summary>
-        private void CreateValueList()
-        {
-            if (this.Params.Input[2].SourceCount == 0)
-            {
-                var parameter = Params.Input[2];
-                // Creates the empty value list
-                GH_ValueList obj = new GH_ValueList();
-                obj.CreateAttributes();
-                obj.ListMode = GH_ValueListMode.DropDown;
-                obj.ListItems.Clear();
-                obj.ToggleItem(1);
-                // Add the items to the value list
-                string[] names = Enum.GetNames(typeof(Transition));
-                int[] values = (int[])Enum.GetValues(typeof(Transition));
-
-                for (int i = 0; i < names.Length; i++)
-                {
-                    obj.ListItems.Add(new GH_ValueListItem(names[i], values[i].ToString()));
-                }
-
-                // Make point where the valuelist should be created on the canvas
-                obj.Attributes.Pivot = new PointF(this.Attributes.Pivot.X - this.Attributes.Bounds.Width - obj.Attributes.Bounds.Width / 4, this.Attributes.Pivot.Y + this.Attributes.Bounds.Height / 2 - obj.Attributes.Bounds.Height);
-                // Add the value list to the active canvas
-                Instances.ActiveCanvas.Document.AddObject(obj, false);
-
-                // Connect the value list to the input parameter
-                parameter.AddSource(obj);
-
-                // Collect data
-                parameter.CollectData();
-
-                // Created
-                _created = true;
-
-                // Expire value list
-                obj.ExpireSolution(true);
-            }
-        }
-        #endregion
+        
         /// <summary>
         /// Gets the exposure of this object in the Graphical User Interface.
         /// </summary>
