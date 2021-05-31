711ad41d988bcf5aad223af27cd93b982a253589
Derk <d.h.bos@tue.nl>
2021-05-31 18:12:13 +0200

    Added a component that changes the startpoint of a closed curve based on a length parameter

---
+++ SaladSlicer/Gh/Components/Geometry/RedefineStartpointLengthComponent.cs
@@ -0,0 +1,119 @@
+﻿// This file is part of SaladSlicer. SaladSlicer is licensed 
+// under the terms of GNU General Public License as published by the 
+// Free Software Foundation. For more information and the LICENSE file, 
+// see <https://github.com/3DCP-TUe/SaladSlicer>.
+
+// System Libs
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+// Grasshopper Libs
+using Grasshopper;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Special;
+using Grasshopper.Kernel.Types;
+using Grasshopper.Kernel.Data;
+// Rhino Libs
+using Rhino.Geometry;
+using SaladSlicer.Core.Geometry;
+using SaladSlicer.Core.Enumerations;
+
+namespace SaladSlicer.Gh.Components.Geometry
+{
+    /// <summary>
+    /// Represents the component that joins curves using linear interpolation. 
+    /// </summary>
+    public class RedefineStartpointLengthComponent : GH_Component
+    {
+        #region fields
+        #endregion
+
+        /// <summary>
+        /// Public constructor without any arguments.
+        /// </summary>
+        public RedefineStartpointLengthComponent()
+          : base("Set Start Point", // Component name
+              "RS", // Component nickname
+              "Redefines the startpoint of a closed curve based on distance along curve.", // Description
+              "Salad Slicer", // Category
+              "Geometry") // Subcategory
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddCurveParameter("Curve", "C", "Curve", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Length", "L", "The length along the curve between the old startpoint and new startpoint of the curve [0 to 1]", GH_ParamAccess.item, 0);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddCurveParameter("Curve", "C", "Curve.", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            // Declare variable of input parameters
+            Curve curve = new Line().ToNurbsCurve();
+            double length = new double();
+
+            // Access the input parameters individually. 
+            if (!DA.GetData(0, ref curve)) return;
+            if (!DA.GetData(1, ref length)) return;
+
+            // Check input values
+            if (length < 0.0) { AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Length input cannot be smaller than 0."); }
+            if (length > curve.GetLength()) { AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Length input is larger than curve length"); }
+            
+            // Create the code line
+            Curve curveCopy = curve.DuplicateCurve();
+            curveCopy.Domain = new Interval(0, curve.GetLength());
+            curveCopy = Curves.SetStartPointAtParam(curveCopy, length);
+            // Assign the output parameters
+            DA.SetData(0, curveCopy);
+        }
+        /// <summary>
+        /// Gets the exposure of this object in the Graphical User Interface.
+        /// </summary>
+        public override GH_Exposure Exposure
+        {
+            get { return GH_Exposure.secondary; }
+        }
+
+        /// <summary>
+        /// Gets whether this object is obsolete.
+        /// </summary>
+        public override bool Obsolete
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Gets the 24x24 pixels icon.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get { return Properties.Resources.ExampleIcon; }
+        }
+
+        /// <summary>
+        /// Gets the ID for this object type. 
+        /// Every object must supply a unique and unchanging ID that is used to identify objects of the same type.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("D2D5D1AE-B933-4C87-ABA8-057C2E9C4E1F"); }
+        }
+
+    }
+}
