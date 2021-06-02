bd73828d29de17901ff1b2cc8e62d2975870b6dd
Derk <d.h.bos@tue.nl>
2021-06-02 09:01:33 +0200

    Created a component that reverses every other curve in a list, starting with the second

---
+++ SaladSlicer/Gh/Components/Geometry/ReverseEveryOtherComponent.cs
@@ -0,0 +1,112 @@
+﻿// This file is part of SaladSlicer. SaladSlicer is licensed 
+// under the terms of GNU General Public License as published by the 
+// Free Software Foundation. For more information and the LICENSE file, 
+// see <https://github.com/3DCP-TUe/SaladSlicer>.
+
+// System Libs
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+// Grasshopper Libs
+using Grasshopper;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Special;
+using Grasshopper.Kernel.Types;
+using Grasshopper.Kernel.Data;
+// Rhino Libs
+using Rhino.Geometry;
+using SaladSlicer.Core.Geometry;
+using SaladSlicer.Core.Enumerations;
+
+namespace SaladSlicer.Gh.Components.Geometry
+{
+    /// <summary>
+    /// Represents the component that joins curves using linear interpolation. 
+    /// </summary>
+    public class ReverseEveryOtherComponent : GH_Component
+    {
+        #region fields
+        #endregion
+
+        /// <summary>
+        /// Public constructor without any arguments.
+        /// </summary>
+        public ReverseEveryOtherComponent()
+          : base("Alternate", // Component name
+              "A", // Component nickname
+              "Reverses the direction of every other curve in a list, startint with the second.", // Description
+              "Salad Slicer", // Category
+              "Geometry") // Subcategory
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddCurveParameter("Curves", "C", "Curves to alternate", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddCurveParameter("Curves", "C", "Alternated curves", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            // Declare variable of input parameters
+            List<Curve> curves = new List<Curve>();
+
+            // Access the input parameters individually. 
+            if (!DA.GetDataList(0, curves)) return;
+
+            // Create the code line
+            List<Curve> curvesCopy = curves.ConvertAll(curve => curve.DuplicateCurve());
+            curvesCopy = Curves.ReverseEveryOther(curvesCopy);
+
+            // Assign the output parameters
+            DA.SetDataList(0, curvesCopy);
+        }
+        /// <summary>
+        /// Gets the exposure of this object in the Graphical User Interface.
+        /// </summary>
+        public override GH_Exposure Exposure
+        {
+            get { return GH_Exposure.secondary; }
+        }
+
+        /// <summary>
+        /// Gets whether this object is obsolete.
+        /// </summary>
+        public override bool Obsolete
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Gets the 24x24 pixels icon.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get { return Properties.Resources.ExampleIcon; }
+        }
+
+        /// <summary>
+        /// Gets the ID for this object type. 
+        /// Every object must supply a unique and unchanging ID that is used to identify objects of the same type.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("1DCEE820-E981-49F7-8E99-2D9BA7B695A9"); }
+        }
+
+    }
+}
