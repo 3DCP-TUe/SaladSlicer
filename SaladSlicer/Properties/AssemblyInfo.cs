36a92c1a764e56d455d356f9349b9674afa4ec22
Arjen Deetman <info@arjendeetman.nl>
2021-06-04 09:31:51 +0200

    Added the Info Component

--- SaladSlicer/Properties/AssemblyInfo.cs
+++ SaladSlicer/Properties/AssemblyInfo.cs
@@ -1,6 +1,5 @@
 ﻿// System Libs
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -28,7 +27,7 @@ using System.Runtime.InteropServices;
 //      Major Version
 //      Minor Version 
 //      Build Number
-//      Revision
+//      Revision <- Salad Slicer doesn't use revision numbers in the public version.
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
