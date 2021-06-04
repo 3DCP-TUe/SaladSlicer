64b8ffd97c417fe145ee02c32eab0aa7da8d0c52
Arjen Deetman <info@arjendeetman.nl>
2021-06-04 09:31:30 +0200

    Small correction in the Absolute Coordinate class

--- SaladSlicer/Core/CodeGeneration/AbsoluteCoordinate.cs
+++ SaladSlicer/Core/CodeGeneration/AbsoluteCoordinate.cs
@@ -67,7 +67,7 @@ namespace SaladSlicer.Core.CodeGeneration
         {
             get
             { 
-                if (_plane != null) { return false; } 
+                if (_plane == null) { return false; } 
                 return true;
             }
         }
