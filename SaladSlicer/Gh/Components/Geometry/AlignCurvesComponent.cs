4681d8826ab92a1e95fd3b3a047909ee1141b762
Arjen Deetman <info@arjendeetman.nl>
2021-08-26 16:29:55 +0200

    Added the align curves component.

---
+++ SaladSlicer/Gh/Components/Geometry/AlignCurvesComponent.cs
@@ -0,0 +1,107 @@
+﻿// This file is part of SaladSlicer. SaladSlicer is licensed 
+// under the terms of GNU General Public License as published by the 
+// Free Software Foundation. For more information and the LICENSE file, 
+// see <https://github.com/3DCP-TUe/SaladSlicer>.
+
+// System Libs
+using System;
+using System.Collections.Generic;
+// Grasshopper Libs
+using Grasshopper.Kernel;
+// Rhino Libs
+using Rhino.Geometry;
+// Using Salad Slicer Libs
+using SaladSlicer.Core.Geometry;
+
+namespace SaladSlicer.Gh.Components.Geometry
+{
+    /// <summary>
+    /// Represents the component that aligns the curves.
+    /// </summary>
+    public class AlignCurvesComponent : GH_Component
+    {
+        #region fields
+        #endregion
+
+        /// <summary>
+        /// Public constructor without any arguments.
+        /// </summary>
+        public AlignCurvesComponent()
+          : base("Align Curves", // Component name
+              "AC", // Component nickname
+              "Aligns a set of curves / orients the direction in the same direction.", // Description
+              "Salad Slicer", // Category
+              "Geometry") // Subcategory
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddCurveParameter("Curves", "C", "Curves as a list with Curves", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddCurveParameter("Curves", "C", "Curves as a list with Curves.", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            // Declare variable of input parameters
+            List<Curve> curves = new List<Curve>();
+
+            // Access the input parameters individually. 
+            if (!DA.GetDataList(0, curves)) return;
+
+            // Create the new curves
+            List<Curve> result = Curves.AlignContours(curves);
+
+            // Assign the output parameters
+            DA.SetDataList(0, result);
+        }
+
+        /// <summary>
+        /// Gets the exposure of this object in the Graphical User Interface.
+        /// </summary>
+        public override GH_Exposure Exposure
+        {
+            get { return GH_Exposure.quarternary; }
+        }
+
+        /// <summary>
+        /// Gets whether this object is obsolete.
+        /// </summary>
+        public override bool Obsolete
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Gets the 24x24 pixels icon.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get { return Properties.Resources.ExampleIcon; }
+        }
+
+        /// <summary>
+        /// Gets the ID for this object type. 
+        /// Every object must supply a unique and unchanging ID that is used to identify objects of the same type.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("5466193D-47FB-4C21-8755-94D8736F3730"); }
+        }
+
+    }
+}
