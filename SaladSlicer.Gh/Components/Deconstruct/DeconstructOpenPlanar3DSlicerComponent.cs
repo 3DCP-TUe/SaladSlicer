f33b9a7688b3b1f2c927bba58ea6e248421dffba
Arjen Deetman <arjendeetman@hotmail.com>
2022-04-18 13:00:12 +0200

    Added the Deconstruct Open Planar 3D slicer component

---
+++ SaladSlicer.Gh/Components/Deconstruct/DeconstructOpenPlanar3DSlicerComponent.cs
@@ -0,0 +1,104 @@
+﻿// This file is part of SaladSlicer. SaladSlicer is licensed 
+// under the terms of GNU General Public License as published by the 
+// Free Software Foundation. For more information and the LICENSE file, 
+// see <https://github.com/3DCP-TUe/SaladSlicer>.
+
+// System Libs
+using System;
+// Grasshopper Libs
+using Grasshopper.Kernel;
+// Salad Slicer Libs
+using SaladSlicer.Slicers;
+using SaladSlicer.Gh.Parameters.Slicers;
+
+namespace SaladSlicer.Gh.Components.CodeGeneration
+{
+    /// <summary>
+    /// Represent a component that deconstruct an Open Planar 3D Slicer object.
+    /// </summary>
+    public class DeconstructOpenPlanar3DSlicerComponent : GH_Component
+    {
+        /// <summary>
+        /// Public constructor without any arguments.
+        /// </summary>
+        public DeconstructOpenPlanar3DSlicerComponent()
+          : base("Deconstruct Open Planar 3D Slicer", // Component name
+              "DCP3D", // Component nickname
+              "Deconstructs a Open Planar 3D Slicer", // Description
+              "Salad Slicer", // Category
+              "Deconstruct") // Subcategory
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddParameter(new Param_OpenPlanar3DSlicer(), "Open Planar 3D", "OP3D", "Open Planar 3D Slicer.", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddMeshParameter("Mesh", "M", "Mesh as a Mesh.", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Distance", "D", "Distance between frames as a Number.", GH_ParamAccess.item);
+            pManager.AddBooleanParameter("Reverse", "R", "Reverse path direction.", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Heights", "H", "Absolute layer heights a list with Numbers.", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            // Declare variable of input parameters
+            OpenPlanar3DSlicer slicer = new OpenPlanar3DSlicer();
+            
+            // Access the input parameters individually. 
+            if (!DA.GetData(0, ref slicer)) return;
+
+            // Assign the output parameters
+            DA.SetData(0, slicer.Mesh);
+            DA.SetData(1, slicer.Distance);
+            DA.SetData(2, slicer.Reverse);
+            DA.SetDataList(3, slicer.Heights);
+        }
+
+        /// <summary>
+        /// Gets the exposure of this object in the Graphical User Interface.
+        /// </summary>
+        public override GH_Exposure Exposure
+        {
+            get { return GH_Exposure.primary; }
+        }
+
+        /// <summary>
+        /// Gets whether this object is obsolete.
+        /// </summary>
+        public override bool Obsolete
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Gets the 24x24 pixels icon.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get { return null; }
+        }
+
+        /// <summary>
+        /// Gets the ID for this object type. 
+        /// Every object must supply a unique and unchanging ID that is used to identify objects of the same type.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("3BB35E2C-0EFD-488D-BE0E-C567C2A1ED99"); }
+        }
+    } 
+}
