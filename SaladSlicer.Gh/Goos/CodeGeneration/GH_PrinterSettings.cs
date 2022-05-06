618fe2046de8c80a59b02ad9432cc067469a3b19
Arjen Deetman <arjendeetman@hotmail.com>
2022-05-06 18:08:03 +0200

    Added the printer settings goo and parameter

---
+++ SaladSlicer.Gh/Goos/CodeGeneration/GH_PrinterSettings.cs
@@ -0,0 +1,258 @@
+﻿// This file is part of SaladSlicer. SaladSlicer is licensed 
+// under the terms of GNU General Public License as published by the 
+// Free Software Foundation. For more information and the LICENSE file, 
+// see <https://github.com/3DCP-TUe/SaladSlicer>.
+
+// Grasshopper Libs
+using Grasshopper.Kernel.Types;
+// Rhino Libs
+using Rhino.Geometry;
+// Salad Slicer Libs
+using SaladSlicer.CodeGeneration;
+using SaladSlicer.Interfaces;
+
+namespace SaladSlicer.Gh.Goos.CodeGeneration
+{
+    /// <summary>
+    /// Represents the GH_PrinterSettings class.
+    /// </summary>
+    public class GH_PrinterSettings : GH_GeometricGoo<PrinterSettings>
+    {
+        #region (de)serialisation
+        //TODO
+        #endregion
+
+        #region constructors
+        /// <summary>
+        /// Initializes an empty instance of the GH_PrinterSettings class.
+        /// </summary>
+        public GH_PrinterSettings()
+        {
+            this.Value = null;
+        }
+
+        /// <summary>
+        /// Initializes a new Printer Settings Goo instance from a Printer Settings instance.
+        /// </summary>
+        /// <param name="printerSettings"> Printer Settings Value to store inside this Goo instance. </param>
+        public GH_PrinterSettings(PrinterSettings printerSettings)
+        {
+            this.Value = printerSettings;
+        }
+
+        /// <summary>
+        /// Returns a complete duplicate of this Goo instance.
+        /// </summary>
+        /// <returns> A duplicate of the Printer Settings Goo. </returns>
+        public override IGH_Goo Duplicate()
+        {
+            if (this.Value == null)
+            {
+                return new GH_PrinterSettings();
+            }
+            else
+            {
+                return new GH_PrinterSettings(this.Value.Duplicate());
+            }
+        }
+
+        /// <summary>
+        /// Returns a complete duplicate of this Goo insance.
+        /// </summary>
+        /// <returns> A duplicate of the Printer Settings Goo instance. </returns>
+        public override IGH_GeometricGoo DuplicateGeometry()
+        {
+            return this.Duplicate() as IGH_GeometricGoo;
+        }
+        #endregion
+
+        #region methods
+        /// <summary>
+        /// Creates a string description of the current instance value.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            if (this.Value == null)
+            {
+                return "Null Printer Settings";
+            }
+            else
+            {
+                return this.Value.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Attempt a cast to type Q.
+        /// </summary>
+        /// <typeparam name="Q"> Type to cast to. </typeparam>
+        /// <param name="target"> Pointer to target of cast. </param>
+        /// <returns> True on success, false on failure. </returns>
+        public override bool CastTo<Q>(ref Q target)
+        {
+            // Check null type
+            if (this.Value == null) 
+            { 
+                target = default;
+                return false;
+            }
+
+            // Cast to Printer Settings
+            if (typeof(Q).IsAssignableFrom(typeof(PrinterSettings)))
+            {
+                target = (Q)(object)this.Value;
+                return true;
+            }
+
+            // Cast to Printer Settings Goo
+            if (typeof(Q).IsAssignableFrom(typeof(GH_PrinterSettings)))
+            {
+                target = (Q)(object)new GH_PrinterSettings(this.Value);
+                return true;
+            }
+
+            // Cast to IProgram
+            if (typeof(Q).IsAssignableFrom(typeof(IProgram)))
+            {
+                target = (Q)(object)this.Value;
+                return true;
+            }
+
+            // Cast to IProgram Goo
+            if (typeof(Q).IsAssignableFrom(typeof(GH_ProgramObject)))
+            {
+                target = (Q)(object)new GH_ProgramObject(this.Value);
+                return true;
+            }
+
+            // Invalid cast
+            target = default;
+            return false;
+        }
+
+        /// <summary>
+        /// Attempt a cast from generic object.
+        /// </summary>
+        /// <param name="source"> Reference to data that should be cast. </param>
+        /// <returns> True on success, false on failure. </returns>
+        public override bool CastFrom(object source)
+        {
+            // Check null type
+            if (source == null) 
+            {
+                return false; 
+            }
+
+            // Cast from Printer Settings
+            if (typeof(PrinterSettings).IsAssignableFrom(source.GetType()))
+            {
+                this.Value = source as PrinterSettings;
+                return true;
+            }
+
+            // Invalid cast
+            return false;
+        }
+        #endregion
+
+        #region properties
+        /// <summary>
+        /// Gets a value indicating whether or not the current value is valid.
+        /// </summary>
+        public override bool IsValid
+        {
+            get
+            {
+                if (this.Value == null) { return false; }
+                return this.Value.IsValid;
+            }
+        }
+
+        /// <summary>
+        /// Gets a string describing the state of "invalidness". 
+        /// If the instance is valid, then this property should return String.Empty.
+        /// </summary>
+        public override string IsValidWhyNot
+        {
+            get
+            {
+                if (this.Value == null) { return "No internal Printer Settings instance"; }
+                if (this.Value.IsValid) { return string.Empty; }
+                return "Invalid Printer Settings instance";
+            }
+        }
+
+        /// <summary>
+        /// Gets a description of the type of the implementation.
+        /// </summary>
+        public override string TypeDescription
+        {
+            get { return "Defines the Printer Settings."; }
+        }
+
+        /// <summary>
+        /// Gets the name of the type of the implementation.
+        /// </summary>
+        public override string TypeName
+        {
+            get { return "Printer Settings"; }
+        }
+        #endregion
+
+        #region transformation methods
+        /// <summary>
+        /// Transforms the object or a deformable representation of the object.
+        /// </summary>
+        /// <param name="xform"> Transformation matrix. </param>
+        /// <returns> Transformed geometry. </returns>
+        public override IGH_GeometricGoo Transform(Transform xform)
+        {
+            if (Value == null)
+            {
+                return null;
+            }
+
+            else if (Value.IsValid == false)
+            {
+                return null;
+            }
+
+            else
+            {
+                return new GH_PrinterSettings(Value);
+            }
+        }
+
+        /// <summary>
+        /// Morph the object or a deformable representation of the object.
+        /// </summary>
+        /// <param name="xmorph"> Spatial deform. </param>
+        /// <returns> Deformed geometry. </returns>
+        public override IGH_GeometricGoo Morph(SpaceMorph xmorph)
+        {
+            return null;
+        }
+        #endregion
+
+        #region preview data
+        /// <summary>
+        /// Compute an aligned boundingbox.
+        /// </summary>
+        /// <param name="xform"> Transformation to apply to geometry for BoundingBox computation. </param>
+        /// <returns> The world aligned boundingbox of the transformed geometry. </returns>
+        public override BoundingBox GetBoundingBox(Transform xform)
+        {
+            return BoundingBox.Empty;
+        }
+
+        /// <summary>
+        /// Gets the boundingbox for this geometry.
+        /// </summary>
+        public override BoundingBox Boundingbox
+        {
+            get { return this.GetBoundingBox(new Transform()); }
+        }
+        #endregion
+    }
+}
---
+++ SaladSlicer.Gh/Parameters/CodeGeneration/Param_PrinterSettings.cs
@@ -0,0 +1,118 @@
+﻿// This file is part of SaladSlicer. SaladSlicer is licensed 
+// under the terms of GNU General Public License as published by the 
+// Free Software Foundation. For more information and the LICENSE file, 
+// see <https://github.com/3DCP-TUe/SaladSlicer>.
+
+// System Libs
+using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+// Grasshopper Libs
+using Grasshopper.Kernel;
+// Salad Slicer Libs
+using SaladSlicer.Gh.Goos.CodeGeneration;
+
+namespace SaladSlicer.Gh.Parameters.CodeGeneration
+{
+    /// <summary>
+    /// Represents the Param_PrinterSettings class
+    /// </summary>
+    public class Param_PrinterSettings : GH_PersistentGeometryParam<GH_PrinterSettings>
+    {
+        /// <summary>
+        /// Initializes a new instance of the Param_PrinterSettings class
+        /// </summary>
+        public Param_PrinterSettings()
+          : base(new GH_InstanceDescription("Printer Settings", // Parameter name
+              "PS", // Component nickname
+              "Defines the Printer Settings.", // Description
+              "Salad Slicer", // Category
+              "Parameters")) // Subcategory)
+        { 
+        }
+
+        #region properties
+        /// <summary>
+        /// Gets the exposure of this object in the Graphical User Interface.
+        /// </summary>
+        public override GH_Exposure Exposure
+        {
+            get { return GH_Exposure.primary; }
+        }
+
+        /// <summary>
+        /// Override this function to supply a custom icon (24x24 pixels). 
+        /// The result of this property is cached, so don't worry if icon retrieval is not very fast.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get { return null; }
+        }
+
+        /// <summary>
+        /// Returns a consistent ID for this object type. 
+        /// Every object must supply a unique and unchanging ID that is used to identify objects of the same type.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("4BE44EC4-6996-4607-B24E-1B701FC86EE7"); }
+        }
+        #endregion
+
+        #region disable pick parameters (we do not allow users to pick parameters)
+        /// <summary>
+        /// Returns a canceled result. 
+        /// Picking items is disabled for this parameter.
+        /// </summary>
+        /// <param name="values"> Empty list. </param>
+        /// <returns> Canceled result. </returns>
+        protected override GH_GetterResult Prompt_Plural(ref List<GH_PrinterSettings> values)
+        {
+            return GH_GetterResult.cancel;
+        }
+
+        /// <summary>
+        /// Returns a canceled result. 
+        /// Picking items is disabled for this parameter. 
+        /// </summary>
+        /// <param name="value"> Null item.  </param>
+        /// <returns> Canceled result. </returns>
+        protected override GH_GetterResult Prompt_Singular(ref GH_PrinterSettings value)
+        {
+            return GH_GetterResult.cancel;
+        }
+
+        /// <summary>
+        /// Returns a hidden tool strip menu item for picking a single item.
+        /// Picking items is disabled for this parameter.
+        /// </summary>
+        /// <returns> A hidden  tool strip menu item. </returns>
+        protected override ToolStripMenuItem Menu_CustomSingleValueItem()
+        {
+            ToolStripMenuItem item = new ToolStripMenuItem
+            {
+                Text = "Not available",
+                Visible = false
+            };
+
+            return item;
+        }
+
+        /// <summary>
+        /// Returns a hidden tool strip menu item for picking multiple items.
+        /// Picking items is disabled for this parameter.
+        /// </summary>
+        /// <returns> A hidden tool strip menu item. </returns>
+        protected override ToolStripMenuItem Menu_CustomMultiValueItem()
+        {
+            ToolStripMenuItem item = new ToolStripMenuItem
+            {
+                Text = "Not available",
+                Visible = false
+            };
+
+            return item;
+        }
+        #endregion
+    }
+}
