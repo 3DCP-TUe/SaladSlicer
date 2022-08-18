a082efb55dae47848a5ccfb0e720ed69a7be905d
Arjen Deetman <info@arjendeetman.nl>
2022-08-18 11:41:24 +0200

    Added the Surface Contour Mapping component

---
+++ SaladSlicer.Gh/Components/Geometry part 1/SurfaceContourMappingComponent.cs
@@ -0,0 +1,127 @@
+﻿// This file is part of SaladSlicer. SaladSlicer is licensed 
+// under the terms of GNU General Public License as published by the 
+// Free Software Foundation. For more information and the LICENSE file, 
+// see <https://github.com/3DCP-TUe/SaladSlicer>.
+
+// System Libs
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+// Grasshopper Libs
+using Grasshopper.Kernel;
+// Rhino Libs
+using Rhino.Geometry;
+// Salad Libs
+using SaladSlicer.Geometry;
+
+namespace SaladSlicer.Gh.Components.Geometry
+{
+    /// <summary>
+    /// Represents the component that maps contours on a surface. 
+    /// </summary>
+    public class SurfaceContourMappingComponent : GH_Component
+    {
+        /// <summary>
+        /// Public constructor without any arguments.
+        /// </summary>
+        public SurfaceContourMappingComponent()
+          : base("Surface Contour Mapping", // Component name
+              "SCM", // Component nickname
+              "Maps a contour on a surface.", // Description
+              "Salad Slicer", // Category
+              "Geometry part 1") // Subcategory
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddSurfaceParameter("Surface", "S", "Surface as a Surface.", GH_ParamAccess.item);
+            pManager.AddIntegerParameter("Number of curves", "N", "Number of curves to create.", GH_ParamAccess.item, 10);
+            pManager.AddIntegerParameter("Number of samples", "S", "Number of sample points along curves.", GH_ParamAccess.item, 10);
+            pManager.AddBooleanParameter("Tranpose", "T", "Indicates whether or not the surface should be tranposed.", GH_ParamAccess.item, false);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddCurveParameter("Curves", "C", "Curves as a list with Curves.", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            // Declare variable of input parameters
+            Surface surface = Surface.CreateExtrusion(new Line(0.0, 0.0, 0.0, 0.0, 0.0, 1.0).ToNurbsCurve(), new Vector3d(1.0, 0.0, 0.0));
+            int count = 10;
+            int samples = 10;
+            bool transpose = false;
+
+            // Access the input parameters individually. 
+            if (!DA.GetData(0, ref surface)) return;
+            if (!DA.GetData(1, ref count)) return;
+            if (!DA.GetData(2, ref samples)) return;
+            if (!DA.GetData(3, ref transpose)) return;
+
+            // Declare the output variables
+            List<Curve> result = new List<Curve>();
+
+            // Create the new curves
+            try
+            {
+                result = Surfaces.ContourMapping(surface, count, samples, transpose);
+            }
+            catch (WarningException w)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, w.Message);
+            }
+            catch (Exception e)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, e.Message);
+            }
+
+            // Assign the output parameters
+            DA.SetDataList(0, result);
+        }
+
+        /// <summary>
+        /// Gets the exposure of this object in the Graphical User Interface.
+        /// </summary>
+        public override GH_Exposure Exposure
+        {
+            get { return GH_Exposure.secondary; }
+        }
+
+        /// <summary>
+        /// Gets whether this object is obsolete.
+        /// </summary>
+        public override bool Obsolete
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Gets the 24x24 pixels icon.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get { return null; }
+        }
+
+        /// <summary>
+        /// Gets the ID for this object type. 
+        /// Every object must supply a unique and unchanging ID that is used to identify objects of the same type.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("006BE303-42E8-4F10-AD5B-84C5705BCD36"); }
+        }
+    }
+}
---
+++ SaladSlicer/Geometry/Surfaces.cs
@@ -0,0 +1,71 @@
+﻿// This file is part of SaladSlicer. SaladSlicer is licensed 
+// under the terms of GNU General Public License as published by the 
+// Free Software Foundation. For more information and the LICENSE file, 
+// see <https://github.com/3DCP-TUe/SaladSlicer>.
+
+// System Libs
+using System.Linq;
+using System.Collections.Generic;
+// Rhino Libs
+using Rhino.Geometry;
+
+namespace SaladSlicer.Geometry
+{
+    /// <summary>
+    /// Represents a static class that contains a number of methods to manipulate, create and obtain data from surfaces.
+    /// </summary>
+    public static class Surfaces
+    {
+        /// <summary>
+        /// Returns a list with curves mapped on a surface. 
+        /// </summary>
+        /// <param name="surface"> The surface to map the contour on. </param>
+        /// <param name="count"> The amount of curves. </param>
+        /// <param name="samples"> The amount of samples along the curve. </param>
+        /// <param name="transpose"> Indicates whether or not the surface should be tranposed. </param>
+        /// <returns> List with mapped curves. </returns>
+        public static List<Curve> ContourMapping(Surface surface, int count, int samples, bool transpose = false)
+        {
+            List<Curve> result = new List<Curve>() { };
+            Surface surfaceCopy = (Surface)surface.Duplicate();
+
+            if (transpose == true)
+            {
+                surfaceCopy = surfaceCopy.Transpose();
+            }
+
+            if (count < 2)
+            {
+                count = 2;
+            }
+
+            if (samples < 2)
+            {
+                samples = 2;
+            }
+
+            double min1 = surfaceCopy.Domain(0).Min;
+            double max1 = surfaceCopy.Domain(0).Max;
+            double min2 = surfaceCopy.Domain(1).Min;
+            double max2 = surfaceCopy.Domain(1).Max;
+
+            double[] range1 = Enumerable.Range(0, count).Select(i => min1 + (max1 - min1) * ((double)i / (count - 1))).ToArray();
+            double[] range2 = Enumerable.Range(0, samples).Select(i => min2 + (max2 - min2) * ((double)i / (samples - 1))).ToArray();
+
+            for (int i = 0; i < range1.Length; i++)
+            {
+                Point3d[] points = new Point3d[range2.Length];
+
+                for (int j = 0; j < range2.Length; j++)
+                {
+                    surfaceCopy.PointAt(range1[i], range2[j]);
+                    points[j] = surfaceCopy.PointAt(range1[i], range2[j]);
+                }
+
+                result.Add(Curve.CreateInterpolatedCurve(points, 3, CurveKnotStyle.Chord));
+            }
+
+            return result;
+        }
+    }
+}
