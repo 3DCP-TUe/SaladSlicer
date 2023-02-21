b9798b2b788cf7511c0bcc7755656ee906a78b6f
Arjen Deetman <arjendeetman@hotmail.com>
2023-02-21 16:36:03 +0100

    Updated the version number

--- SaladSlicer.Gh/Properties/AssemblyInfo.cs
+++ SaladSlicer.Gh/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("3DCP Research Group at Eindhoven University of Technology")]
 [assembly: AssemblyProduct("Salad Slicer")]
-[assembly: AssemblyCopyright("Copyright © 2021-2022")]
+[assembly: AssemblyCopyright("Copyright © 2021-2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
